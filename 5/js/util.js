d63ea58a8321f531aef74f4ad5eeffd2fc45af1e
Keks <keks@htmlacademy.ru>
2023-03-28 06:20:08 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/util.js
@@ -0,0 +1,21 @@
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createRandomIdGenerator = (min, max) => {
+  const ids = Array.from({length: (max - min)}, (_, index) => index + 1);
+  return () => {
+    const randomIndex = getRandomPositiveInteger(min, max);
+    const [randomId] = ids.splice(randomIndex, 1);
+    return randomId;
+  };
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+export {getRandomArrayElement,
+  getRandomPositiveInteger,
+  createRandomIdGenerator};
